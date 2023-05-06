79e50e769e326819569561a8d2e65d6c49af3b84
yuk6ra <yuki.imamura.all@gmail.com>
2023-05-06 11:13:53 +0900

    Update

--- src/components/CustomWeb3Auth.jsx
+++ src/components/CustomWeb3Auth.jsx
@@ -1,83 +1,44 @@
-import { Button, Card, CardBody, CardHeader, Center, Text } from '@chakra-ui/react'
+import { Button, Card, CardBody, CardHeader, Center, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
-import { Web3Auth } from "@web3auth/modal";
+// import { Web3Auth } from "@web3auth/modal";
 // import { ethers } from "ethers";
 
 const CustomWeb3Auth = () => {
 
     const handleTest = async () => {
 
-        // const ethers = window.ethers;
-
-        // const web3auth = new Web3Auth({
-        //     clientId: "BGEXcv9LdMrzGAYlBufDI0MWzgeGGXi5jN_zqIf_nYAKhDUDCF2xQZJ2zPISVQH-pm1LY3iXG7IChwteVCOdI", // get it from Web3Auth Dashboard
-        //     web3AuthNetwork: "Testnet",
-        //     chainConfig: {
-        //         chainNamespace: "eip155",
-        //         chainId: "80001", // hex of 80001, polygon testnet
-        //         rpcTarget: "https://rpc.ankr.com/polygon_mumbai",
-        //         // Avoid using public rpcTarget in production.
-        //         // Use services like Infura, Quicknode etc
-        //         displayName: "Polygon Mumbai Testnet",
-        //         blockExplorer: "https://mumbai.polygonscan.com/",
-        //         ticker: "MATIC",
-        //         tickerName: "Matic",
-        //     },
-        // });
-
-        // await web3auth.initModal();
-
-        // const web3authProvider = web3auth.connect();
-
-        // const provider = new ethers.providers.Web3Provider(web3authProvider); // web3auth.provider
-
-        // const signer = provider.getSigner();
-
-        // // Get user's Ethereum public address
-        // const address = await signer.getAddress();
-
-        // // Get user's balance in ether
-        // const balance = ethers.utils.formatEther(
-        //     await provider.getBalance(userAddress) // Balance is in wei
-        // );
-
-        // console.log("address", address)
-        // console.log("balance", balance)
-
-        const web3auth = new Web3Auth({
-            clientId: "YOUR_WEB3AUTH_CLIENT_ID", // Get your Client ID from Web3Auth Dashboard
-            chainConfig: {
-              chainNamespace: "eip155",
-              chainId: "0x89", // Use 0x13881 for Mumbai Testnet
-            },
-          });
-          
-          await web3auth.initModal();
-
-          await web3auth.connect();
-
-
-
-          
     }
 
 
     return (
-        <Center>
+
+
+        <Flex
+            h={'100vh'}
+            justifyContent={'center'}
+            alignItems={'center'}
+        >
+
             <Card
                 mt={10}
+                boxShadow={'xl'}
             >
-                <CardHeader>Header</CardHeader>
+                <CardHeader>
+                    <Center>
+                        Let's get started
+                    </Center>
+                </CardHeader>
                 <CardBody>
                     <Button
+                        colorScheme='green'
                         onClick={handleTest}
                     >
-                        create
+                        LINE Login
                     </Button>
                 </CardBody>
             </Card>
 
-        </Center>
+        </Flex>
     )
 }
 
